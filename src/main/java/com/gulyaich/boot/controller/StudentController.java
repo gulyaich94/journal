b3b35dbdfe7a5660b426c4f80f26fa5de77beed8
Разгуляева Анастасия Дмитриевна OUT <Razgulyaeva.An.Dm@sberbank.ru>
2019-06-13 14:16:22 +0300

    Раскидан шаблон реакт, на главной выводится компонент показа списка студентов из результатов реста

--- src/main/java/com/gulyaich/boot/controller/StudentController.java
+++ src/main/java/com/gulyaich/boot/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.gulyaich.boot.entity.Student;
 import com.gulyaich.boot.exception.StudentNotFoundException;
 import com.gulyaich.boot.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,14 +20,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/student")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
     @GetMapping("/")
-    public List<Student> getStudents() {
-        return studentService.getStudents();
+    public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(studentService.getStudents(), HttpStatus.OK);
     }
 
     @PostMapping("/create")
