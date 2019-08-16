a872b07e48067cdb27ecf3bb67743c95c4a12718
Разгуляева Анастасия Дмитриевна OUT <Razgulyaeva.An.Dm@sberbank.ru>
2019-08-16 15:35:52 +0300

    Kafka with ReplyTo

--- src/main/java/com/gulyaich/boot/kafka/listener/KafkaConsumer.java
+++ src/main/java/com/gulyaich/boot/kafka/listener/KafkaConsumer.java
@@ -1,10 +1,10 @@
 package com.gulyaich.boot.kafka.listener;
 
 import com.gulyaich.boot.entity.News;
-import com.gulyaich.boot.exception.NewsErrorSaveException;
 import com.gulyaich.boot.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,13 +14,16 @@ public class KafkaConsumer {
     private NewsService newsService;
 
     @KafkaListener(topics = "kafka_news", groupId = "news_group")
-    public void consume(News news) {
+    @SendTo("kafka_news_response")
+    public News consume(News news) {
         System.out.println("Message consumed " + news);
         try {
             newsService.save(news);
-        } catch (NewsErrorSaveException e) {
-            System.out.println("News don't save " + news);
-
+            news.setStatus("DONE");
+        } catch (Exception e) {
+            System.out.println("News don't save " + news + ". Reason: " + e);
+            news.setStatus("ERROR");
         }
+        return news;
     }
 }
